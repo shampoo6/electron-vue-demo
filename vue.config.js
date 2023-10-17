3d9550cb63d8c0ff9fd2430b9caba8026ca1b200
shampoo6 <shampoo6@163.com>
2023-10-17 14:08:05 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,15 @@ module.exports = defineConfig({
           target: 'nsis',
           icon: 'build/icons/icon.png'
         },
+        // 打包 nsis 格式配置
+        nsis: {
+          // 应用名称
+          // 若要自动更新，则很重要，需要改为如下名称
+          artifactName: "${name}-Setup-${version}.${ext}",
+          deleteAppDataOnUninstall: true,
+          oneClick: false,
+          allowToChangeInstallationDirectory: true,
+        },
         // 拷贝资源到输出 resources 中
         extraResources: [
           'build',
